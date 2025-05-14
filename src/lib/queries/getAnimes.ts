87f0d33c4107d5ba27110ee2a7ac9bb0f8c82715
jefersonpriimer <jefersonpriimer@gmail.com>
2025-05-14 16:54:06 +0000

    chore: modify query

--- src/lib/queries/getAnimes.ts
+++ src/lib/queries/getAnimes.ts
@@ -2,11 +2,37 @@
 import { gql } from "@apollo/client";
 
 export const GET_ANIMES = gql`
-  query GetAnimes {
+  query {
     animes {
       id
+      publicCode
+      slug
       name
+      audioType
       imagePoster
+      synopsis
+      rating
+      score
+      genres {
+        name
+      }
+      totalEpisodes
+      seasons{
+      	seasonNumber
+      }
+
+      episodes{
+        title
+        duration
+        updatedAt
+        synopsis
+      }
+
+      contentSources {
+        authors
+        title
+        sourceType
+      }
     }
   }
 `;
