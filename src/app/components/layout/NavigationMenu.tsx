cd0ac5b514ee3e7561b938368bdc2f466aae87c7
jefersonprimer <jefersonpriimer@gmail.com>
2025-06-07 07:44:30 -0300

    fix: link of /calendar to /simulcasts

--- src/app/components/layout/NavigationMenu.tsx
+++ src/app/components/layout/NavigationMenu.tsx
@@ -54,7 +54,7 @@ export default function NavigationMenu() {
         <Link href="/videos/popular" className={styles.navLink}>Populares</Link>
       </li>
       <li className={styles.navItem}>
-        <Link href="/calendar" className={styles.navLink}>Simulcasts</Link>
+        <Link href="/simulcasts" className={styles.navLink}>Simulcasts</Link>
       </li>
       <li className={styles.navItem}>
         <div
